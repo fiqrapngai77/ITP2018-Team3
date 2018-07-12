5930b895bea3333691deb4407d869a16479ee3da
desmondtayweechuan <1602049@sit.singaporetech.edu.sg>
2018-07-13 01:18:06 +0800

    Rename Syafiq portion
    termite -> flying termite
    fly -> house fly

---
+++ Images to be trained/Final Version/train/renameXMLTagContent.py
@@ -0,0 +1,40 @@
+from xml.etree import ElementTree as et
+import os, glob
+
+#testFormat
+#FORMAT = "C:\\tensorflow1\\models\\research\\object_detection\\images\\test\\"
+
+#trainFormat
+
+OLD_FORMAT = "termite"
+
+NEW_FORMAT = "flying termite"
+
+##OLD_FORMAT = "fly"
+##
+##NEW_FORMAT = "house fly"
+
+
+'''Get Current directory'''
+cwd = os.getcwd()
+'''Set current directory to execute'''
+os.chdir(cwd)
+
+count = 1
+
+'''Loop thru the folder'''
+for file in glob.glob("*"):
+    if file.endswith(".xml"):
+        tree = et.parse(file)
+        root = tree.getroot()
+
+        for name in root.iter('name'):
+            if name.text == OLD_FORMAT:
+                new_name = NEW_FORMAT
+                name.text = str(new_name)
+                tree.write(file)        
+                print (file + " : " + str(count))
+                count = count + 1
+
+
+print ("Done")
